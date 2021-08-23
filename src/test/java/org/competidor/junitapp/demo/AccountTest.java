371396f4dc3129b6bac3d80fb66b3297a01511a8
Elena Piaggio <elenapiaggio@gmail.com>
2021-08-23 14:28:41 +0200

    ADD test for InsufficientMoney

--- src/test/java/org/competidor/junitapp/demo/AccountTest.java
+++ src/test/java/org/competidor/junitapp/demo/AccountTest.java
@@ -1,60 +1,73 @@
 package org.competidor.junitapp.demo;
 
+import org.competidor.junitapp.demo.exceptions.InsufficientMoneyException;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
-class CuentaTest {
+class AccountTest {
     @Test
     void testNameCuenta(){
-        // primero instanciar la clase a probar
-        Cuenta cuenta = new Cuenta("Elenita", new BigDecimal("1000.12344"));
+        // First you have to instantiate the class to test
+        Account account = new Account("Elenita", new BigDecimal("1000.12344"));
 
         String esperado = "Elenita";
-        String real = cuenta.getPersona();
+        String real = account.getPerson();
 
-        assertNotNull(cuenta.getSaldo());
+        assertNotNull(account.getBalance());
         assertEquals(esperado, real);
         assertTrue(real.equals("Elenita"));
     }
 
     @Test
     void testSaldoCuenta() {
-        Cuenta cuenta = new Cuenta("Elenita", new BigDecimal("1000.12344") );
-        assertNotNull(cuenta.getSaldo());
-        assertEquals(1000.12344, cuenta.getSaldo().doubleValue());
+        Account account = new Account("Elenita", new BigDecimal("1000.12344") );
+        assertNotNull(account.getBalance());
+        assertEquals(1000.12344, account.getBalance().doubleValue());
 
         // assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO) < 0);
-        assertTrue(cuenta.getSaldo().compareTo(BigDecimal.ZERO) > 0);
+        assertTrue(account.getBalance().compareTo(BigDecimal.ZERO) > 0);
     }
 
     @Test
     void testReferenciaCuenta() {
-        Cuenta cuenta1 = new Cuenta("John Doe", new BigDecimal("8900.9997") );
-        Cuenta cuenta2 = new Cuenta("John Doe", new BigDecimal("8900.9997") );
+        Account account1 = new Account("John Doe", new BigDecimal("8900.9997") );
+        Account account2 = new Account("John Doe", new BigDecimal("8900.9997") );
 
         //assertNotEquals(cuenta2, cuenta1);
-        assertEquals(cuenta2, cuenta1);
+        assertEquals(account2, account1);
     }
 
     @Test
     void testDebidoCuenta() {
-        Cuenta cuenta = new Cuenta("Elena", new BigDecimal("1000.12345"));
-        cuenta.debito(new BigDecimal(100));
-        assertNotNull(cuenta.getSaldo());
-        assertEquals(900, cuenta.getSaldo().intValue());
-        assertEquals("900.12345", cuenta.getSaldo().toPlainString());
+        Account account = new Account("Elena", new BigDecimal("1000.12345"));
+        account.debit(new BigDecimal(100));
+        assertNotNull(account.getBalance());
+        assertEquals(900, account.getBalance().intValue());
+        assertEquals("900.12345", account.getBalance().toPlainString());
     }
 
     @Test
     void testCreditoCuenta() {
-        Cuenta cuenta = new Cuenta("Elena", new BigDecimal("1000.12345"));
-        cuenta.credito(new BigDecimal(100));
-        assertNotNull(cuenta.getSaldo());
-        assertEquals(1100, cuenta.getSaldo().intValue());
-        assertEquals("1100.12345", cuenta.getSaldo().toPlainString());
+        Account account = new Account("Elena", new BigDecimal("1000.12345"));
+        account.credit(new BigDecimal(100));
+        assertNotNull(account.getBalance());
+        assertEquals(1100, account.getBalance().intValue());
+        assertEquals("1100.12345", account.getBalance().toPlainString());
+    }
+
+    @Test
+    void testInsufficientMoneyException() {
+        Account account = new Account("Elena", new BigDecimal("1000.12345"));
+        Exception exception = assertThrows( InsufficientMoneyException.class, ()-> {
+            account.debit(new BigDecimal(1500));
+        } );
+
+        String actual = exception.getMessage();
+        String expected = "Insufficient money";
+
+        assertEquals(expected, actual);
     }
 }
